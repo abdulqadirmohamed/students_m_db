afc9d9fc701d593a50956dfb55f2805910ca01df
abdulqadir876 <hindiseconsulting@gmail.com>
2023-11-07 17:15:41 +0300

    main

--- index.php
+++ index.php
@@ -7,8 +7,27 @@ include 'conn.php'
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css"/>
 </head>
 <body>
     <h1>PHP</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Class</th>
+                <th>Phone</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Name</td>
+                <td>Class</td>
+                <td>Phone</td>
+            </tr>
+        </tbody>
+    </table>
 </body>
 </html>
